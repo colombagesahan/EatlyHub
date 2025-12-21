0ef7e53d2e40bd95bdeb9150d92f9e86a19421d2
colombagesahan <colombagesahan@gmail.com>
2025-12-21 20:16:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
-const CACHE_NAME = 'eatlyhub-v3-pro';
+const CACHE_NAME = 'eatlyhub-v4-app';
 const ASSETS = [
+  './',
   './index.html',
+  './loging.html',
+  './home.html',
   './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/3075/3075977.png'
+  'https://cdn-icons-png.flaticon.com/512/3075/3075977.png',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
